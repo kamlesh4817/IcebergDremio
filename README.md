72c8eb34722e47f4f373ae794f4bb2391a642e11
kamlesh-dremio <95704397+kamlesh-dremio@users.noreply.github.com>
2024-10-29 21:13:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+
+# Iceberg on ADLS / S3 / MINIO with Dremio via Hive, Hadoop, or Glue catalog
+
+This demo project shows how to use Dremio to query Apache Iceberg tables on ADLS, S3, or MINIO.
+
+## Quick start
+
+- Clone the project, cd into the directory and run `docker-compose up -d`. This will spin up jupyter notebook, dremio and minio.
+- You may need to restart the container for createMinioSource if you do not see any sources in Dremio
+- Go to http://localhost:8888 and open the work/demo 1.ipynb notebook and run it. 
+- Once you have created a table and performed some operations, open http://localhost:9047 (username: dremio, password: dremio123) to login to Dremio and query the hive source. Minio can be browsed at http://localhost:9000 (username: minioadmin, password: minioadmin)
+
+## Installation
+
+Install project with docker-compose
+
+Step 1: Create or use an existing Azure Storage Account with Data Lake Storage, AWS S3 account or use the local MINIO provided
+
+Step 2: Obtain storage your account secrect
+
+Step 4: clone repo
+
+```bash
+git clone https://github.com/dremio-brock/IcebergDremio.git
+cd IcebergDremio
+```
+
+Step 3: Copy conf/metastore-site-default.xml  and rename to conf/metastore-site.xml
+Uncomment the type of storage you wish to use and fill in your account information
+
+*Note on first run with Minio, it will automatically create a bucket with a  account key and secret minioadmin:minioadmin
+   
+Step 4: Build and run
+
+```bash
+docker-compose build 
+docker-compose up -d
+```
+
+Step 5: Jupyter Notebook \
+\
+Login to your notebook from the web browser localhost:8888 \
+\
+Step 6: Create a copy of config-sample.ini as config.ini and update it with your values\
+\
+In the work directory, modify config.ini with your settings. If you are using minio, no changes are needed.\
+\
+If you wish to demo loading streaming data from an api such as the one used here you will need to obtain your own api keys.
+
+
+## Setup Dremio
+
+- Open dremio by going to the url localhost:9047
+- A default username and password dremio:dremio123 has been created already
+- The Dremio startup script enables iceberg automatically as described here: http://docs.dremio.com/data-formats/iceberg-enabling/
+
+## Sources
+By default a Minio source has been created and added as well as the hive source. Steps to do so are outlined here: http://docs.dremio.com/data-sources/s3/#configuring-s3-for-minio\
+The default minio credentials are minioadmin:minioadmin\
+\
+**When querying from the hive source, the catalog should be set to iceberg.catalog_type=hive (default)\
+**When using a file based source, the catalog should be set to iceberg.catalog_type=hadoop (default)
+
+## Notes
+
+After Dremio is setup, you can begin querying Iceberg tables. \
+\
+*Notes*
+- By default Dremio Dremio will read the new Iceberg metadata file every 60 seconds.
+- If a table does not show up when browsing a source, this may be due to metadata collection, either turn down the timer on the source, or query the new dataset directly in a new query window.
+
+
+## Create some Iceberg Tables
+Work with Iceberg using Spark. I have simplified the configuration and setup of spark using the python file called iceberg_spark.py. When you wish to use spark, you can import and call the function within. 
+
+Demo 1: 
+- Begin working with the code to learn how to create spark tables and perform some operations. 
+
+Demo 2: 
+- Run micro batches to insert data into an iceberg tables 
+
+Dremio:
+- While working with your iceberg tables, you can use Dremio to start query the results. Those results can then be used in external tools such as Tableau, Power BI, dbeaver, etc by connecting directly to Drmeio. 
+- Additionally you can build out a semantic layer in Dremio joining many datasets and providing your tools with a single view of the data. 
---
+++ notebook/iceberg_spark.py
@@ -0,0 +1,129 @@
+from os import environ
+import findspark
+from pyspark import sql, SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from py4j.java_gateway import java_import
+import time
+import configparser
+import socket
+findspark.init()
+
+
+def start_spark(storage, config_section, catalog_type):
+    """
+    Create a spark with the defaults set for a specific storage type and iceberg catalog settings
+    :param storage: adls, s3, minio
+    :param config_section: the section to look for storage settings
+    :param catalog_type: hive, hadoop, or glue catalog
+    """
+    # add Iceberg dependency
+    ICEBERG_VERSION="0.12.1"
+    DEPENDENCIES="org.apache.iceberg:iceberg-spark3-runtime:{}".format(ICEBERG_VERSION)
+
+    # Read config file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    
+    # initialize sparkConf
+    conf = SparkConf()
+    
+    # Set warehouse location
+    warehouse = config[config_section]['warehouse']
+    
+    # dummy hive_uri
+    hive_uri = None
+    
+    if storage == 'minio':
+        ACCESS_KEY_ID = config[config_section]['ACCESS_KEY_ID']
+        SECRET_ACCESS_KEY = config[config_section]['SECRET_ACCESS_KEY']
+        
+        # get the endpoint ip address, spark doesn't like docker hostnames
+        ENDPOINT = 'http://' + socket.gethostbyname('minio') + ':9000'
+        
+        # add dependencies
+        DEPENDENCIES+=",org.apache.hadoop:hadoop-aws:3.2.0"
+        DEPENDENCIES+=",com.amazonaws:aws-java-sdk-bundle:1.11.375"
+        
+        # add spark conf
+        conf.set("spark.hadoop.fs.s3a.access.key", ACCESS_KEY_ID)
+        conf.set("spark.hadoop.fs.s3a.secret.key", SECRET_ACCESS_KEY)
+        conf.set("spark.hadoop.fs.s3a.endpoint", ENDPOINT)
+        conf.set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+    elif storage == 's3' and catalog_type == 'glue':
+      
+        # Get AWS credentials
+        ACCESS_KEY_ID = config[config_section]['ACCESS_KEY_ID']
+        SECRET_ACCESS_KEY = config[config_section]['SECRET_ACCESS_KEY']
+        AWS_REGION = config[config_section]['AWS_REGION']
+        
+        # set AWS env variables
+        environ["AWS_ACCESS_KEY_ID"] = ACCESS_KEY_ID
+        environ["AWS_SECRET_ACCESS_KEY"] = SECRET_ACCESS_KEY
+        environ["AWS_REGION"] = AWS_REGION
+        
+        # add dependencies
+        DEPENDENCIES+=",software.amazon.awssdk:bundle:2.15.40"
+        DEPENDENCIES+=",software.amazon.awssdk:url-connection-client:2.15.40"
+        
+        # add spark conf
+        conf.set("spark.sql.catalog.spark_catalog","org.apache.iceberg.spark.SparkCatalog")
+        conf.set("spark.sql.catalog.spark_catalog.io-impl","org.apache.iceberg.aws.s3.S3FileIO")
+        conf.set("spark.sql.catalog.spark_catalog","org.apache.iceberg.spark.SparkCatalog")
+        conf.set("spark.sql.catalog.spark_catalog.catalog-impl","org.apache.iceberg.aws.glue.GlueCatalog")
+        conf.set("spark.sql.catalog.spark_catalog.lock-impl","org.apache.iceberg.aws.glue.DynamoLockManager")
+        conf.set("spark.sql.catalog.spark_catalog.lock.table","myGlueLockTable")
+        
+    elif storage == 's3' and catalog_type in ['hadoop','hive']:
+        print('Using S3 with Hive or Hadoop')
+        # Get AWS credentials
+        ACCESS_KEY_ID = config[config_section]['ACCESS_KEY_ID']
+        SECRET_ACCESS_KEY = config[config_section]['SECRET_ACCESS_KEY']
+        
+        # add dependencies
+        DEPENDENCIES+=",org.apache.hadoop:hadoop-aws:3.2.0"
+        DEPENDENCIES+=",com.amazonaws:aws-java-sdk-bundle:1.11.375"
+        
+        # add spark conf
+        conf.set("spark.hadoop.fs.s3a.access.key", ACCESS_KEY_ID)
+        conf.set("spark.hadoop.fs.s3a.secret.key", SECRET_ACCESS_KEY)
+
+    elif storage == 'adls':
+        spark.conf.set("fs.azure.account.key." + storageAccountName + ".blob.core.windows.net", secret)
+        
+        # add dependencies
+        DEPENDENCIES+=",org.apache.hadoop:hadoop-azure:3.2.0"
+        DEPENDENCIES+=",com.microsoft.azure:azure-storage:7.0.0" 
+        DEPENDENCIES+=",org.apache.hadoop:hadoop-azure-datalake:3.2.0"
+    else:
+        print('you use a storage type of minio, s3, or adls')
+    
+    if catalog_type == 'hive':
+        hive_uri = config[config_section]['HIVE_URI']
+        conf.set("spark.sql.catalog.spark_catalog.type", "hive")
+        conf.set("spark.sql.catalog.spark_catalog.uri", hive_uri)
+    elif catalog_type == 'hadoop':
+        conf.set("spark.sql.catalog.spark_catalog.type", "hadoop")
+
+
+    # set environment dependencies
+    environ['PYSPARK_SUBMIT_ARGS'] = '--packages {} pyspark-shell'.format(DEPENDENCIES)
+
+    # Set iceberg settings
+    conf.set('spark.jars.packages', DEPENDENCIES)
+    conf.set("spark.sql.catalog.spark_catalog.warehouse", warehouse)
+    conf.set("spark.sql.execution.pyarrow.enabled", "true")
+    conf.set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
+    conf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    sc = spark.sparkContext
+    jvm = sc._gateway.jvm
+
+    # add jars for java operations
+    java_import(jvm, "org.apache.iceberg.CatalogUtil")
+    java_import(jvm, "org.apache.iceberg.catalog.TableIdentifier")
+    java_import(jvm, "org.apache.iceberg.Schema")
+    java_import(jvm, "org.apache.iceberg.types.Types")
+    java_import(jvm, "org.apache.iceberg.PartitionSpec")
+    java_import(jvm, "org.apache.iceberg.actions.Actions")
+    return spark, jvm, hive_uri, sc
